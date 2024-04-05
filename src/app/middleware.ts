79209de905c86fc1bc5f20d5b3f0d6cb84ee8b83
ben <me@benspace.xyz>
2024-04-05 20:28:06 +0700

    feat: implement auth middleware

---
+++ src/app/middleware.ts
@@ -0,0 +1,22 @@
+import { withAuth } from "next-auth/middleware";
+
+// middleware is applied to all routes, use conditionals to select
+
+export default withAuth(function middleware(req) {}, {
+  callbacks: {
+    authorized: ({ req, token }) => {
+      const pathname = req.nextUrl.pathname;
+      if (
+        pathname.startsWith("/admin") &&
+        (token?.role == "Guest" || !token?.role)
+      ) {
+        return false;
+      } else if (pathname == "/admin/users" && token?.role != "SuperAdmin") {
+        return false;
+      } else if (pathname.startsWith("/dashboard") && !token?.email) {
+        return false;
+      }
+      return true;
+    },
+  },
+});
