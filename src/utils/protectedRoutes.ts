1e38abeb1e5106900b798b51339f294950bc8953
ben <me@benspace.xyz>
2024-04-08 05:06:56 +0700

    refactor: protected routes

---
+++ src/utils/protectedRoutes.ts
@@ -0,0 +1,42 @@
+import { Roles } from "@prisma/client";
+import { IconType } from "react-icons";
+
+export interface ProtectedRoutes {
+  title: string;
+  path: string;
+  regex: RegExp;
+  roles: (Roles | "SubOrgan")[] | "All";
+}
+
+export const protectedRoutes: ProtectedRoutes[] = [
+  {
+    title: "Post",
+    path: "/admin/posts",
+    regex: /\/admin\/posts(\/|)[A-Za-z]?/i,
+    roles: "All",
+  },
+  {
+    title: "Link Shortener",
+    path: "/admin/link",
+    regex: /\/admin\/link(\/|)/i,
+    roles: "All",
+  },
+  {
+    title: "Formulir",
+    path: "/admin/form",
+    regex: /\/admin\/forms(\/|)[A-Za-z]?/i,
+    roles: "All",
+  },
+  {
+    title: "Sub-Organ Setup",
+    path: "/admin/sub-organ",
+    regex: /\/admin\/sub-organ(\/|)[A-Za-z]?/i,
+    roles: "All",
+  },
+  {
+    title: "Users",
+    path: "/admin/users",
+    regex: /\/admin\/users(\/|)[A-Za-z]?/i,
+    roles: ["SuperAdmin"],
+  },
+];
