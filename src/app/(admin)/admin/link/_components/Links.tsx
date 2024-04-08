ec6bfc71ee054c54c93173256b16ea1f7c603b30
Teguh Bayu Pratama <teguhbayupratama@gmail.com>
2024-04-08 20:27:04 +0800

    modified:   src/app/(admin)/admin/link/_components/Links.tsx

--- src/app/(admin)/admin/link/_components/Links.tsx
+++ src/app/(admin)/admin/link/_components/Links.tsx
@@ -4,7 +4,7 @@ import LinkFigure from "./LinkFigure";
 export default function Links({ links }: { links: LinkWithCountAndUser[] }) {
   return (
     <div className="flex flex-col gap-3">
-      {links.map((link:LinkWithCountAndUser) => (
+      {links.map((link: LinkWithCountAndUser) => (
         <LinkFigure link={link} key={link.slug}></LinkFigure>
       ))}
     </div>
