0060aaa928c1d21fad0a1f7f534b691ff09de7f6
Teguh Bayu Pratama <teguhbayupratama@gmail.com>
2024-04-08 16:28:19 +0800

    feat(backend): Add linkshortener query
    Co-authored-by: Kusindra Aji Rabbany <work@benspace.xyz>

---
+++ src/utils/database/linkShortener.query.ts.ts
@@ -0,0 +1,37 @@
+import { Prisma } from "@prisma/client";
+import prisma from "@/lib/prisma";
+
+export const findAllLinks = async (
+  filter?: Prisma.Link_ShortenerWhereInput,
+) => {
+  return prisma.link_Shortener.findMany({
+    where: filter,
+    orderBy: { created_at: "desc" },
+    include: {
+      user: { select: { name: true } },
+      count: { select: { click_count: true } },
+    },
+  });
+};
+
+
+export const findLink = async (filter: Prisma.Link_ShortenerWhereInput) =>{
+  return prisma.link_Shortener.findFirst({
+    where: filter,
+    include: {
+      user: { select: { name: true } },
+      count: { select: { click_count: true } },
+    },
+  });
+}
+
+export const createLink = async (data: Prisma.Link_ShortenerUncheckedCreateInput) =>{
+  return prisma.link_Shortener.create({data})
+}
+
+export const updateLink = async (
+  where: Prisma.Link_ShortenerWhereUniqueInput,
+  update: Prisma.Link_ShortenerUncheckedUpdateInput,
+) => {
+  return await prisma.link_Shortener.update({ where, data: update });
+};
