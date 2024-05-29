20401b01a64f7b383e23a16fe8a0047a1bea037b
Ahsan <ahsanaz461@gmail.com>
2024-05-29 21:15:07 +0700

    fix: Use margin instead of transform's translate

--- src/app/_components/global/Navbar.tsx
+++ src/app/_components/global/Navbar.tsx
@@ -79,7 +79,7 @@ export default function Navbar() {
         </button>
       </div>
       <div
-        className={`block xl:hidden h-[300px] w-full z-[800] bg-white transition-all duration-500 ${isExpanded ? "translate-y-0" : " -translate-y-96"}`}
+        className={`block xl:hidden h-[300px] w-full z-[800] bg-white transition-all duration-500 ${isExpanded ? "mt-0" : " -mt-96"}`}
       >
         <div className="flex flex-col gap-8 text-start justify-start items-start my-[21px] ms-[20px] lg:ms-[52px]">
           {navOptions.map((navOption) => (
@@ -88,6 +88,7 @@ export default function Navbar() {
               href={navOption.href}
               // Splitted "/a/b" will form an array: ["", "a", "b"], that's why we use the second index as comparation
               className={`rounded-full text-center text-[16px] transition-all duration-300 hover:text-primary-400 ${pathname.split("/")[1] === navOption.href.split("/")[1] ? "text-red-400" : ""}`}
+              onClick={() => setIsExpanded(false)}
             >
               {navOption.title}
             </Link>
