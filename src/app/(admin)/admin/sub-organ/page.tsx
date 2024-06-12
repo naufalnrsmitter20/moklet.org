ec7ada9d3154242ff8556dfb0b88147f00065dec
Teguh Bayu Pratama <teguhbayupratama@gmail.com>
2024-06-12 08:23:05 +0700

    fix: make subor page an actual component

--- src/app/(admin)/admin/sub-organ/page.tsx
+++ src/app/(admin)/admin/sub-organ/page.tsx
@@ -1,9 +1,9 @@
 import { H1 } from "@/app/_components/global/Text";
 
 export default function SuborAdmin() {
-    return (
-        <>
-            <H1>TOLONG DISLICE</H1>
-        </>
-    )
+  return (
+    <>
+      <H1>TOLONG DISLICE</H1>
+    </>
+  );
 }
