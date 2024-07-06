cff791920d5b71abd06f245d6ef73895e9e6bec1
naufalnrsmitter20 <x3mnaufalnabilramadhan@gmail.com>
2024-07-06 22:39:39 +0700

    feat: change description text from change requested

--- src/app/(main)/tentang/page.tsx
+++ src/app/(main)/tentang/page.tsx
@@ -9,47 +9,36 @@ export default function Tentang() {
           <div className="flex flex-col gap-[18px] mb-6 text-wrap">
             <H1>Tentang Moklet.org</H1>
             <P>
-              Selamat datang di Moklet.org, portal yang didedikasikan untuk
-              mengeksplorasi kreativitas dan inovasi di SMK Telkom Malang.
+              Selamat datang di Moklet.org, platform yang berfungsi sebagai
+              portal kreativitas dan inovasi organisasi di SMK Telkom Malang.
               Moklet.org berfungsi sebagai jendela menuju kegiatan kreatif dan
               inovatif yang dilakukan oleh berbagai organisasi dan
-              sub-organisasi di lingkungan sekolah ini. Di sini, Anda akan
-              menemukan berbagai informasi dan berita terbaru yang menggambarkan
-              semangat kolaborasi dan dedikasi siswa dalam berbagai kegiatan.
+              sub-organisasi di lingkungan Moklet. Moklet.org juga berfungsi
+              sebagai sistem informasi dan platform berbagi berita-berita
+              tentang berbagai kegiatan yang dilakukan Mokleters.
             </P>
           </div>
           <div className="flex flex-col gap-[18px] mb-6 text-wrap">
             <P>
-              Melalui halaman ini, Anda dapat mengakses berita terbaru mengenai
-              kegiatan organisasi dan sub-organisasi di Moklet. Dari acara-acara
-              besar hingga kegiatan sehari-hari, kami menyajikan informasi
-              terkini yang menunjukkan dinamika dan perkembangan yang terjadi di
-              lingkungan sekolah. Selain itu, Anda juga bisa mengetahui lebih
-              dalam tentang peran penting OSIS dan MPK dalam memimpin dan
-              mengorganisir berbagai kegiatan, serta bagaimana mereka membentuk
-              kepemimpinan siswa di Moklet.
+              Melalui Moklet.org, publik dapat mengakses berita terbaru mengenai
+              kegiatan-kegiatan organisasi dan sub-organisasi di Moklet. Dari
+              acara-acara besar hingga kegiatan sehari-hari, informasi yang
+              menunjukkan dinamika dan perkembangan yang terjadi di lingkungan
+              sekolah. Selain itu, publik dapat mengetahui lebih dalam tentang
+              peran penting OSIS dan MPK dalam memimpin dan mengorganisir
+              berbagai kegiatan dalam SMK Telkom Malang, serta bagaimana mereka
+              membentuk kepemimpinan siswa di Moklet.
             </P>
           </div>
           <div className="flex flex-col gap-[18px] mb-6 text-wrap">
             <P>
-              Moklet.org juga memberikan wadah bagi pengembangan bakat siswa
-              melalui berbagai sub-organisasi di bawah naungan OSIS. Di sini,
-              siswa dapat menemukan dan mengasah potensi terbaik mereka dalam
-              lingkungan yang inspiratif dan mendukung. Beragam kegiatan yang
-              ditawarkan oleh sub-organisasi ini memungkinkan siswa untuk
-              mengekspresikan diri dan berkembang secara optimal, baik dalam
-              bidang akademik maupun non-akademik.
-            </P>
-          </div>
-          <div className="flex flex-col gap-[18px] mb-6 text-wrap">
-            <P>
-              Tidak hanya itu, Moklet.org juga menyediakan ruang bagi siswa
-              untuk menyampaikan aspirasi mereka demi masa depan yang lebih
-              baik. Melalui platform ini, siswa dapat berbagi impian dan harapan
-              mereka, yang akan menjadi masukan berharga bagi MPK dalam
-              merancang dan mengembangkan program-program yang lebih baik. Kami
-              percaya bahwa setiap aspirasi memiliki kekuatan untuk mewujudkan
-              perubahan positif dan mendorong kemajuan bersama.
+              Moklet.org juga menyediakan ruang bagi siswa untuk menyampaikan
+              aspirasi mereka melalui fitur formulirnya. Dengan ini, siswa dapat
+              berbagi impian dan harapan mereka, yang akan menjadi masukan
+              berharga bagi MPK dalam merancang dan mengembangkan
+              program-program yang lebih baik. Kami percaya bahwa setiap
+              aspirasi memiliki kekuatan untuk mewujudkan perubahan positif dan
+              mendorong kemajuan bersama.
             </P>
           </div>
         </div>
