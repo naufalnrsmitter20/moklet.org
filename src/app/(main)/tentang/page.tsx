a88dc4cc78ae2fc0ea5d171c9e0d480abc74ae6d
ben <me@benspace.xyz>
2024-07-11 20:41:11 +0700

    update metadata

--- src/app/(main)/tentang/page.tsx
+++ src/app/(main)/tentang/page.tsx
@@ -3,36 +3,34 @@ import { SectionWrapper } from "@/app/_components/global/Wrapper";
 
 export default function Tentang() {
   return (
-    <>
-      <SectionWrapper id="tentang">
-        <div>
-          <div className="flex flex-col gap-[18px] mb-6 text-wrap">
-            <H1>Tentang Moklet.org</H1>
-            <P>
-              Moklet.org merupakan sebuah portal kreativitas dan inovasi
-              organisasi di SMK Telkom Malang (Moklet). Moklet.org berfungsi
-              sebagai jendela menuju kegiatan kreatif dan inovatif yang
-              dilakukan oleh berbagai organisasi dan sub-organisasi di
-              lingkungan Moklet. Portal ini juga berfungsi sebagai sistem
-              informasi dan platform untuk berbagi berita tentang berbagai
-              kegiatan yang dilakukan oleh para Mokleters.
-              <br />
-              <br />
-              Moklet.org bertujuan untuk memperkenalkan berbagai organisasi di
-              SMK Telkom Malang kepada publik, menunjukkan betapa beragam dan
-              aktifnya lingkungan organisasi di sekolah ini, serta bagaimana
-              setiap kegiatan dan program yang dijalankan dapat memberikan
-              manfaat bagi siswa.
-              <br />
-              <br />
-              Proyek Moklet.org juga bersifat open-source, yang berarti semua
-              siswa SMK Telkom Malang dapat memberikan kontribusi mereka pada
-              proyek ini dan mendapatkan kredit sebagai kontributor pada laman
-              Kontributor sebagai bentuk penghargaan.
-            </P>
-          </div>
+    <SectionWrapper id="tentang">
+      <div>
+        <div className="flex flex-col gap-[18px] mb-6 text-wrap">
+          <H1>Tentang Moklet.org</H1>
+          <P>
+            Moklet.org merupakan sebuah portal kreativitas dan inovasi
+            organisasi di SMK Telkom Malang (Moklet). Moklet.org berfungsi
+            sebagai jendela menuju kegiatan kreatif dan inovatif yang dilakukan
+            oleh berbagai organisasi dan sub-organisasi di lingkungan Moklet.
+            Portal ini juga berfungsi sebagai sistem informasi dan platform
+            untuk berbagi berita tentang berbagai kegiatan yang dilakukan oleh
+            para Mokleters.
+            <br />
+            <br />
+            Moklet.org bertujuan untuk memperkenalkan berbagai organisasi di SMK
+            Telkom Malang kepada publik, menunjukkan betapa beragam dan aktifnya
+            lingkungan organisasi di sekolah ini, serta bagaimana setiap
+            kegiatan dan program yang dijalankan dapat memberikan manfaat bagi
+            siswa.
+            <br />
+            <br />
+            Proyek Moklet.org juga bersifat open-source, yang berarti semua
+            siswa SMK Telkom Malang dapat memberikan kontribusi mereka pada
+            proyek ini dan mendapatkan kredit sebagai kontributor pada laman
+            Kontributor sebagai bentuk penghargaan.
+          </P>
         </div>
-      </SectionWrapper>
-    </>
+      </div>
+    </SectionWrapper>
   );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,12 +15,13 @@ const robots =
 
 export const metadata: Metadata = {
   title: {
-    default: "Moklet's Organization",
+    default: "Moklet's Organization - SMK Telkom Malang",
     template: "%s | Moklet.org",
   },
   description:
-    "A one doorway to explore Moklet's organizations' creativity and innovations",
-  keywords: "moklet.org, Moklet, Moklet.org, Telkom, SMK, Malang",
+    "A one doorway to explore Moklet's (SMK Telkom Malang) organizations' creativity and innovations",
+  keywords:
+    "moklet.org, Moklet, Moklet.org, Telkom, SMK, Malang, SMK Telkom Malang",
   authors: { name: "MokletDev", url: "https://dev.moklet.org" },
   creator: "MokletDev Team",
   publisher: "SMK Telkom Malang",
