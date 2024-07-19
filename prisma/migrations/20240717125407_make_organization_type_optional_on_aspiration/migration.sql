220b9018f8f089dc38400812506127484bf73709
ben <me@benspace.xyz>
2024-07-19 09:24:34 +0700

    fix: change table names

--- prisma/migrations/20240717125407_make_organization_type_optional_on_aspiration/migration.sql
+++ prisma/migrations/20240717125407_make_organization_type_optional_on_aspiration/migration.sql
@@ -1,2 +1,2 @@
 -- AlterTable
-ALTER TABLE `aspirasi` MODIFY `organisasi` ENUM('OSIS', 'MPK', 'BDI', 'PALWAGA', 'PASKATEMA', 'TSBC', 'TSFC', 'TSVC', 'PMR', 'MEMO', 'MAC', 'METIC', 'COMET', 'PUSTEL', 'DA') NULL;
+ALTER TABLE `Aspirasi` MODIFY `organisasi` ENUM('OSIS', 'MPK', 'BDI', 'PALWAGA', 'PASKATEMA', 'TSBC', 'TSFC', 'TSVC', 'PMR', 'MEMO', 'MAC', 'METIC', 'COMET', 'PUSTEL', 'DA') NULL;
